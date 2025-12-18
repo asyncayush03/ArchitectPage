dfc1b15da85e20e6f913a6d5d39fa09b539cead6
asyncayush03 <ayushsomi2003@gmail.com>
2025-12-18 17:21:10 +0530

    prod 3

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const instance = axios.create();
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
+});
 
 instance.interceptors.response.use(
-  res => res,
-  err => {
+  (res) => res,
+  (err) => {
     if (err.response?.status === 401) {
       localStorage.removeItem("token");
       window.location.href = "/login";
